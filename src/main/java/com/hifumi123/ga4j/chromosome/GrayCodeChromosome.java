ca389f40a39a51c2e0f8362ecff60375aa81c55f
Hifumi123 <you@example.com>
2022-08-24 10:47:32 +0800

    在 GrayCodeChromosome 类中添加允许设置基因数组的新构造方法，修复 GrayCodeChromosome 类的 decode 方法解码错误的问题

--- src/main/java/com/hifumi123/ga4j/chromosome/GrayCodeChromosome.java
+++ src/main/java/com/hifumi123/ga4j/chromosome/GrayCodeChromosome.java
@@ -11,18 +11,22 @@ public class GrayCodeChromosome extends BinaryChromosome {
 	public GrayCodeChromosome(int length) {
 		super(length);
 	}
+	
+	public GrayCodeChromosome(boolean[] genes) {
+		super(genes);
+	}
 
 	@Override
 	public int decode(int begin, int end) {
 		//格雷码转成二进制码
-		boolean[] bs = new boolean[getLength()];
-		bs[0] = getGene(0);
+		boolean[] bs = new boolean[end - begin];
+		bs[0] = getGene(begin);
 		for (int i = 1; i < bs.length; i++)
-			bs[i] = bs[i - 1] ^ getGene(i);
+			bs[i] = bs[i - 1] ^ getGene(begin + i);
 		
 		int r = 0;
 		int o = 1;
-		for (int i = end - 1; i >= begin; i--) {
+		for (int i = bs.length - 1; i >= 0; i--) {
 			if (bs[i])
 				r += o;
 			
